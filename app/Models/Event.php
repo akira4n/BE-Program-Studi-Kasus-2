d924eb347d20516fb84fb561b8c138807ff580a8
akira4n <yobiijo94@gmail.com>
2025-12-25 00:43:28 +0700

    feat(models): fillable field anf relation

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'capacity',
+        'stock',
+        'start_event',
+        'price',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'quantity',
+        'total_price',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }

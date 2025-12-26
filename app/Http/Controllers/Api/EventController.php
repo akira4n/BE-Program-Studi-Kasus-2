7b7bafe00a26324cae2448f42ac892d86345f8a0
akira4n <yobiijo94@gmail.com>
2025-12-26 10:58:14 +0700

    feat: event controller

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $event = Event::all();
+
+        return response()->json([
+            'data' => $event,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'capacity' => 'required|integer',
+            'start_event' => 'required|date_format:Y-m-d',
+            'price' => 'required|numeric',
+        ]);
+
+        $event = Event::create([
+            'user_id' => Auth::id(),
+            'stock' => $validated['capacity'],
+            ...$validated,
+        ]);
+
+        return response()->json([
+            'message' => 'Data event berhasil dibuat',
+            'data' => $event,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $event = Event::findOrFail($id);
+
+        return response()->json([
+            'data' => $event
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $event = Event::findOrFail($id);
+
+        if ($event->user_id !== Auth::id() && Auth::user()->role !== 'admin') {
+            return response()->json([
+                'message' => 'Forbidden: Anda tidak memiliki akses hak untuk mengupdate data ini',
+            ], 403);
+        }
+
+        $validated = $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'capacity' => 'sometimes|integer',
+            'start_event' => 'sometimes|date_format:Y-m-d',
+            'price' => 'sometimes|numeric',
+        ]);
+
+        if ($request->has('capacity')) {
+            $soldTickets = $event->transactions()
+                ->whereIn('status', ['paid', 'waiting'])
+                ->sum('quantity');
+
+            $newCapacity = $request->capacity;
+
+            if ($newCapacity < $soldTickets) {
+                return response()->json([
+                    'message' => "Kapasitas baru ($newCapacity) tidak boleh lebih kecil dari tiket yang sudah terjual ($soldTickets)"
+                ], 422);
+            }
+
+            $event->stock = $newCapacity - $soldTickets;
+        }
+
+        $event->update($validated);
+
+        return response()->json([
+            'message' => 'Data event berhasil diperbarui',
+            'data' => $event,
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $event = Event::findOrFail($id);
+
+        if ($event->user_id !== Auth::id() && Auth::user()->role !== 'admin') {
+            return response()->json([
+                'message' => 'Forbidden: Anda tidak memiliki akses hak untuk menghapus data ini',
+            ], 403);
+        }
+
+        $event->delete();
+
+        return response()->json([
+            'message' => 'Data event berhasi dihapus'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,11 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/events', [EventController::class, 'index']);
+    Route::get('/events/{id}', [EventController::class, 'show']);
+
+    Route::post('/events', [EventController::class, 'store']);
+    Route::patch('/events/{id}', [EventController::class, 'update']);
+});
